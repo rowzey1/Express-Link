9d4f1079a9f46ed83f1a01fab7e9d72da5d15c5c
Rosy Basil <basilkalurosy@gmail.com>
2024-11-18 18:15:47 -0500

    put database in gitignore

---
+++ .gitignore/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://basilkalurosy:Rosy7412@rosyb.pt1jk.mongodb.net/Chat?retryWrites=true&w=majority&appName=RosyB', 
+    'dbName': 'RosyB'
+};
